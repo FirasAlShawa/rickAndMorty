7acb1029ad3e04aab55cce2693832b98c03db9cd
FerasTawuniya <Firas.alshawa@gmail.com>
2024-04-03 01:09:31 +0300

    clean up the project

---
+++ lib/rick_and_morty_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
