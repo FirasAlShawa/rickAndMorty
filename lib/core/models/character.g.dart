a1468708298b8212fcaf4435fae8bbb440073e0d
FerasTawuniya <Firas.alshawa@gmail.com>
2024-04-14 12:48:22 +0300

    add character model

---
+++ lib/core/models/character.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'character.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Character _$CharacterFromJson(Map<String, dynamic> json) => Character(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      status: json['status'] as String,
+      species: json['species'] as String,
+      type: json['type'] as String,
+      gender: json['gender'] as String,
+      image: json['image'] as String,
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$CharacterToJson(Character instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'status': instance.status,
+      'species': instance.species,
+      'type': instance.type,
+      'gender': instance.gender,
+      'image': instance.image,
+      'url': instance.url,
+    };
