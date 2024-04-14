2389df6529b908e84267a4fee908ba6af73cb1a6
FerasTawuniya <Firas.alshawa@gmail.com>
2024-04-14 12:55:41 +0300

    update main and app class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rick_and_morty/rick_and_morty_app.dart';
 
 void main() {
-  runApp(const RickAndMortyApp());
+  runApp(
+    const ProviderScope(
+      child: RickAndMortyApp(),
+    ),
+  );
 }
--- lib/rick_and_morty_app.dart
+++ lib/rick_and_morty_app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/characters/views/characters_view.dart';
 
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Placeholder(),
+    return MaterialApp(
+      home: CharactersView(),
     );
   }
 }
